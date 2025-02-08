347c2e7a18d0397ec0932f258938c0e0b6717fd1
sotapio <kitune25252@gmail.com>
2025-02-08 18:07:28 +0900

    package.jsonと依存関係の追加、react-three-fiberの型定義ファイルを追加

--- react-sample-app/src/App.js
+++ react-sample-app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from "@react-three/fiber"
+import { useRef } from "react"
+
+function Box({ props }) {
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color='hotpink' />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='canvas-container'>
+      <Canvas>
+        <mesh>
+          <Box />
+          <boxGeometry args={[2, 2, 2]} />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
