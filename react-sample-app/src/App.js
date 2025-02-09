dda23ba8c8252861b34259f4e5d6eea95d469f4b
sotapio <kitune25252@gmail.com>
2025-02-09 13:14:52 +0900

    Boxコンポーネントにテクスチャを追加し、回転処理を修正

--- react-sample-app/src/App.js
+++ react-sample-app/src/App.js
@@ -1,54 +1,55 @@
-import './App.css';
-import { Canvas,useFrame } from "@react-three/fiber"
-import { useRef, useState } from "react"
-import { config , useSpring , animated } from "@react-spring/three"
+import "./App.css";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { config, useSpring, animated } from "@react-spring/three";
+import { TextureLoader } from "three";
 
 function Box(props) {
   const ref = useRef();
   const [clicked, setClicked] = useState(false);
   const [hovered, setHovered] = useState(false);
 
+  // useLoader を使用してテクスチャをロード
+  const texture = useLoader(TextureLoader, "https://threejsfundamentals.org/threejs/resources/images/wall.jpg");
+
   // 回転させる
-  useFrame(() => (ref.current.rotation.x += 0.01));
+  useFrame(() => {
+    if (ref.current) ref.current.rotation.x += 0.01;
+  });
 
   const { scale } = useSpring({
     scale: clicked ? 2 : 1,
     config: config.wobbly,
   });
-  
+
   return (
-    // クリックしたら大きさが変わる
     <animated.mesh
       {...props}
       ref={ref}
       onClick={() => setClicked(!clicked)}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
-      scale={ scale }> 
-
-      {/* ボックスの大きさを指定している */}
+      scale={scale.to((s) => [s, s, s])}
+    >
       <boxGeometry args={[1, 1, 1]} />
-      {/* ボックスの色を指定している */}
       <meshStandardMaterial 
-        color={hovered ? "yellow" : "pink"} 
-        emissive={hovered ? "yellow" : "pink"} 
-        emissiveIntensity={0.5} 
+        map={texture} 
       />
     </animated.mesh>
-  )
+  );
 }
 
 function App() {
   return (
     <>
-      <div id='canvas-container'>
+      <div id="canvas-container">
         <Canvas>
-            {/* ２つのボックスの位置をずらす */}
-            <Box position={[-1.6,0,0]} />
-            <Box position={[1.6, 0, 0]} />
-            <ambientLight intensity={0.5} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
+          {/* ２つのボックスの位置をずらす */}
+          <Box position={[-1.6, 0, 0]} />
+          <Box position={[1.6, 0, 0]} />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
         </Canvas>
       </div>
       <h1>Three.js Fiber</h1>
